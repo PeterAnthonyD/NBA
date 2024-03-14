5b1aab3a7f6f46751bd7fa4dc67e602d4f15b0dd
Peter Dola <Peteradola@gmail.com>
2024-03-14 17:34:29 -0500

    v1.1

---
+++ TodaysGames.R
@@ -0,0 +1,10 @@
+url <- "https://www.basketball-reference.com/leagues/NBA_2024_games-march.html"
+webpage <- read_html(url)
+nba_games_df <- webpage %>% html_table(header = TRUE, fill = TRUE) %>% .[[1]]
+
+
+nba_games_df$Date <- as.Date(nba_games_df$Date, format = "%a, %b %e, %Y")
+
+nba_gamees_today = subset(nba_games_df, nba_games_df$Date==Sys.Date())
+
+nba_gamees_today <- nba_gamees_today[, colnames(nba_gamees_today) != ""]
--- TotalPoints.R
+++ TotalPoints.R
@@ -1,10 +1,101 @@
 library(nbastatR)
-
-nba_schedule = seasons_schedule(seasons = 2024, season_types = "Regular Season")
-
+library(dplyr)
+library(fuzzyjoin)
+library(zoo)
+#Define Model
+game_logs <- game_logs(seasons = 2024, 
+                       result_types = 'team', 
+                       season_types = "Regular Season")
 
 nba_games<-game_logs(seasons = 2024, result_types = "team",
               season_types = "Regular Season", nest_data = F,
               assign_to_environment = TRUE, return_message = TRUE)
 
-nba_games<-
+nba_games$pointsAgainst <- nba_games$ptsTeam - nba_games$plusminusTeam
+
+nba_games <- nba_games %>%
+  arrange(dateGame) %>%  # Replace 'date_column' with the actual date column
+  group_by(nameTeam) %>%
+  mutate(last_five = zoo::rollmean(ptsTeam, k = 5, fill = NA, align = "right"))
+
+nba_games <- nba_games %>%
+  arrange(dateGame) %>%  # Replace 'date_column' with the actual date column
+  group_by(nameTeam) %>%
+  mutate(last_five_against = zoo::rollmean(pointsAgainst, k = 5, fill = NA, align = "right"))
+
+nba_quickModle<-lm(ptsTeam ~ as.factor(slugTeam) + 
+                     as.factor(slugOpponent) + 
+                     last_five +
+                     last_five_against, 
+                   data = nba_games)
+summary(nba_quickModle)
+
+nba_teams_lookup <- nba_games %>% select(slugTeam, nameTeam, last_five, dateGame, last_five_against)
+nba_teams_lookup <- nba_teams_lookup %>%
+  group_by(nameTeam) %>%
+  filter(dateGame == max(dateGame))
+
+
+nba_teams_lookup <- unique(nba_teams_lookup)
+
+nba_gamees_today_home <-  nba_gamees_today %>% select("Home/Neutral",
+                            "Visitor/Neutral")
+nba_gamees_today_home <- nba_gamees_today_home %>% rename(nameTeam = `Home/Neutral`)
+
+# Perform fuzzy matching left join
+nba_gamees_today_home_join <- stringdist_join(nba_gamees_today_home, nba_teams_lookup,
+                                              by = 'nameTeam', mode = 'left', method = "jw",
+                                              max_dist = 99, distance_col = 'dist') %>%
+  group_by('nameTeam') %>%
+  slice_min(order_by = dist, n = 1)
+
+nba_gamees_today_home_join <- nba_gamees_today_home_join %>% select(`Visitor/Neutral`,  slugTeam)
+
+nba_gamees_today_home_join <- nba_gamees_today_home_join %>% rename(nameTeam = `Visitor/Neutral`, 
+                                                                    HomeAbbriv = "slugTeam")
+
+nba_gamees_today_home_join <- stringdist_join(nba_gamees_today_home_join, nba_teams_lookup, by = 'nameTeam', mode = 'left', method = "jw", max_dist = 99, distance_col = 'dist') %>%
+  group_by('nameTeam') %>%
+  slice_min(order_by = dist, n = 1)
+
+nba_gamees_today_home_join <- nba_gamees_today_home_join %>% select(HomeAbbriv,
+                                                                    slugTeam, last_five, last_five_against)
+
+nba_gamees_today_home_join <- nba_gamees_today_home_join %>% rename(slugOpponent = slugTeam)
+nba_gamees_today_home_join  <- nba_gamees_today_home_join %>% rename(slugTeam = HomeAbbriv)
+
+firstscore_pred = predict.lm(nba_quickModle, nba_gamees_today_home_join,
+                                                   interval = c("prediction"),
+                                                   level = .95)
+nba_gamees_today_home_join$firstscore = firstscore_pred[,"fit"]
+nba_gamees_today_home_join$firstup = firstscore_pred[,"upr"]
+nba_gamees_today_home_join$firstlow = firstscore_pred[,"lwr"]
+
+
+nba_gamees_today_home_join_second <- nba_gamees_today_home_join %>% rename(slugTeam_2 = slugOpponent)
+nba_gamees_today_home_join_second <- nba_gamees_today_home_join_second %>% rename(slugOpponent_2 = slugTeam)
+nba_gamees_today_home_join_second <-  nba_gamees_today_home_join_second %>% rename(slugTeam = slugTeam_2,
+                                                                                    slugOpponent = slugOpponent_2)
+
+
+SecondScore_pred = predict.lm(nba_quickModle, nba_gamees_today_home_join_second,
+                              interval = c("prediction"),
+                              level = .95)
+
+nba_gamees_today_home_join_second$secondscore <-SecondScore_pred[,"fit"]
+nba_gamees_today_home_join_second$secondup <-SecondScore_pred[,"upr"]
+nba_gamees_today_home_join_second$secondlwr <-SecondScore_pred[,"lwr"]
+
+predictions_martch_13 <- nba_gamees_today_home_join_second %>%
+  select(slugOpponent, slugTeam, firstscore, firstup, firstlow, secondscore, secondup, secondlwr)
+
+predictions_martch_13$total_pred = predictions_martch_13$firstscore+
+  predictions_martch_13$secondscore
+
+predictions_martch_13$total_pred_lwr = predictions_martch_13$firstlow+
+  predictions_martch_13$secondlwr
+
+predictions_martch_13$total_pred_up = predictions_martch_13$firstup+
+  predictions_martch_13$secondup
+
+write.csv(predictions_martch_13, file = "predictions_march_14.csv")
